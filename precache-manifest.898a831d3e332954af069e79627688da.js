a27c655639a81c183d7474834f534af2bdf08144
Vinicius Santana <santana@Viniciuss-MacBook-Pro.local>
2018-11-27 18:46:51 -0800

    Updates

---
+++ precache-manifest.898a831d3e332954af069e79627688da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a009ae182a91ec3814d",
+    "url": "/react-tagcreator/static/css/main.82a5251b.chunk.css"
+  },
+  {
+    "revision": "5a009ae182a91ec3814d",
+    "url": "/react-tagcreator/static/js/main.5a009ae1.chunk.js"
+  },
+  {
+    "revision": "e5f1bd10652fc6b90e34",
+    "url": "/react-tagcreator/static/js/1.e5f1bd10.chunk.js"
+  },
+  {
+    "revision": "07f10d52f37b6f6ea913",
+    "url": "/react-tagcreator/static/js/runtime~main.07f10d52.js"
+  },
+  {
+    "revision": "ddb8940270e79ad94c6b156a82199077",
+    "url": "/react-tagcreator/index.html"
+  }
+];
